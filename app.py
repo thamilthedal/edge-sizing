33fe31e0e9c771e751a5206d4fef6f82489320d2
thamilthedal <thamilthedal@gmail.com>
2023-06-23 01:24:34 +0530

    finalise Merge

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 import streamlit as st
-<<<<<<< HEAD
-from calc import calculate_y, calculate_smallD, calculate_y_water, calculate_BF
 import extra_streamlit_components as stx
+from calc import calculate_y, calculate_smallD, calculate_y_water, calculate_BF
 
 title = st.title(r"$\Delta x_{min}(L, N_D, BF, y^+)$")
 
 caption = st.caption(
     "Calculates the smallest cell size for given $y^+$ and mesh details for ANSYS Meshing.")
-=======
-from calc import calculate_y, calculate_smallD, calculate_BF
->>>>>>> 68665702c885a43f8e5e12842d94272293b9b203
 
 st.divider()
 
@@ -69,23 +65,16 @@ ND = col2.text_input("Number of divisions to be made:", key='ND', value='0')
 bf = col3.text_input("Bias-factor to apply:", key='bf', value='0.0')
 
 if st.button("Calculate"):
-<<<<<<< HEAD
     if chosen_id == "tab1":
         result = calculate_y_water(float(T), float(
             P), float(G), float(L), float(yPlus_Water))
     else:
         result = calculate_y(float(rho), float(U), float(
             L), float(mu), float(yPlus_General))
-=======
-	result = calculate_y(float(rho), float(U), float(L), float(mu), float(yPlus))
-	result2 = calculate_smallD(float(L)/2, int(ND), float(bf))
-	result3 = calculate_BF(float(L)/2, int(ND), result)
->>>>>>> 68665702c885a43f8e5e12842d94272293b9b203
 
     result2 = calculate_smallD(float(L)/2, int(ND), float(bf))
     result3 = calculate_BF(float(L)/2, int(ND), result)
 
-<<<<<<< HEAD
     col1, col2 = st.columns(2)
     with st.container():
         col1.success(r'$\Delta x_{min}$ for the $y^+$ is ' + f'{result:{3}.{5}}' + ' m')
@@ -99,44 +88,34 @@ if st.button("Calculate"):
             st.error(
                 r'Recommended bias factor is too high. Consider changing mesh settings.')
 
-with st.expander(r"Code for calculating $\Delta x_{min}$ for the mesh"):
+with st.expander(r'Code for calculating $\Delta x_{min}$ for the mesh'):
+
     code = '''def calculate_smallD(L, ND, bf):
-=======
-	with st.container():
-		st.success(r'Recommended bias factor for the given $y^+$ could be '+ f'{result3:{1}.{4}}')
+    sum = 0
+    gr = bf**(1.0/(ND-1))
+    for i in range(ND):
+        sum += gr**i
+    L1 = L/sum
+    
+    return L1'''
 
-with st.expander(r"Code for calculating $\Delta x_{min}$ for the mesh"):
-	code = '''def calculate_smallD(L, ND, bf):
->>>>>>> 68665702c885a43f8e5e12842d94272293b9b203
-	sum = 0
-	gr = bf**(1.0/(ND-1))
-	for i in range(ND):
-	    sum += gr**i
-	L1 = L/sum
-	
-	return L1'''
-
-<<<<<<< HEAD
     st.code(code, language='python')
 
-with st.expander(r'Code for calculating recommended bias factor'):
+
+with st.expander('Code for calculating recommended bias factor'):
+
     code = '''def calculate_BF(L, ND, L1):
-=======
-	st.code(code, language='python')
+    print(L, ND, L1)    
+    test_list = list(np.arange(1, 31, 0.001))
+    dummy = calculate_smallD(L, ND, np.arange(1, 31, 0.001))
+    error = abs(dummy - L1)/L1
+    index = np.where(error == min(error))[0][0]
+    
+    print(test_list[index])
 
+    if test_list[index] < 30:
+        return test_list[index]
+    else:
+        return 0.0'''
 
-with st.expander(r'Code for calculating recommended bias factor'):
-	code = '''def calculate_BF(L, ND, L1):
->>>>>>> 68665702c885a43f8e5e12842d94272293b9b203
-	test_list = list(np.arange(1, 31, 0.001))
-	dummy = calculate_smallD(L, ND, np.arange(1, 31, 0.001))
-	error = abs(dummy - L1)/L1
-	index = np.where(error == min(error))[0][0]
-	
-	return test_list[index]'''
-
-<<<<<<< HEAD
     st.code(code, language='python')
-=======
-	st.code(code, language='python')
->>>>>>> 68665702c885a43f8e5e12842d94272293b9b203
--- calc.py
+++ calc.py
@@ -38,14 +38,11 @@ def calculate_y_water(T, P, G, L, yPlus):
 	U = G/state.rho
 	return calculate_y(state.rho, U, L, state.mu, yPlus)
 
-def calculate_BF(L, ND, L1):
-	print(L, ND, L1)	
+def calculate_BF(L, ND, L1):	
 	test_list = list(np.arange(1, 31, 0.001))
 	dummy = calculate_smallD(L, ND, np.arange(1, 31, 0.001))
 	error = abs(dummy - L1)/L1
 	index = np.where(error == min(error))[0][0]
-	
-	print(test_list[index])
 
 	if test_list[index] < 30:
 		return test_list[index]
