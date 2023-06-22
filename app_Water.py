1db5d35096f476ecdaa50b4bc521f542da1bed84
thamilthedal <thamilthedal@gmail.com>
2023-06-22 15:07:53 +0530

    initial commit

---
+++ app_Water.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from calc import calculate_y_water, calculate_smallD
+
+title = st.title(r"$y_d(T, P, L, G, y^+)$ for Water")
+
+T = st.text_input("Temperature (K):", key='T', value = '0.0')
+
+P = st.text_input("Pressure ($Pa$):", key = 'P', value = '0.0')
+
+L = st.text_input("Characteristic length of the flow ($m$):", key = 'L', value = '0.0')
+
+G = st.text_input("Mass velocity of the flow ($kg/m^2 s$):", key = 'U', value = '0.0')
+
+ND = st.text_input("Number of divisions to be made:", key = 'ND', value = '0')
+
+bf = st.text_input("Bias-factor to apply:", key = 'bf', value = '0.0')
+
+yPlus = st.text_input("$y^+$ value near the wall:", key = 'yPlus', value = '0.0')
+
+if st.button("Calculate"):
+	result = calculate_y_water(float(T), float(P), float(G), float(L), float(yPlus))
+	result2 = calculate_smallD(float(L)/2, int(ND), float(bf))
+	st.write(f'$y_d$ near the wall is {result:{3}.{5}} m')
+	st.write(f'$y_d$ for the proposed mesh is {result2:{3}.{5}} m')
---
+++ calc.py
@@ -0,0 +1,40 @@
+import numpy as np
+from iapws import IAPWS97
+
+def calculate_Re(rho, U, L, mu):
+	return (rho*U*L)/mu
+
+def calculate_skin_friction(Re):
+
+	# Schlichting Skin-Friction Coefficient
+	# Valid for Re_x < 10^9
+	return (2*np.log10(Re) - 0.65)**(-2.3)
+
+def calculate_wall_shear(cf, U, rho):
+	return cf*0.5*rho*(U**2)
+
+def calculate_ustar(tw, rho):
+	return np.sqrt(tw/rho)
+
+def calculate_y(rho, U, L, mu, yPlus):
+	Re = calculate_Re(rho, U, L, mu)
+	cf = calculate_skin_friction(Re)
+	tw = calculate_wall_shear(cf, U, rho)
+	ustar = calculate_ustar(tw, rho)
+
+	return (yPlus*mu)/(rho*ustar)
+
+def calculate_smallD(L, ND, bf):
+	sum = 0
+	gr = bf**(1.0/(ND-1))
+	for i in range(ND):
+	    sum += gr**i
+	L1 = L/sum
+	
+	return L1
+
+def calculate_y_water(T, P, G, L, yPlus):
+	state = IAPWS97(P=P/1e6, T=T)
+	U = G/state.rho
+	print(state.rho, U)
+	return calculate_y(state.rho, U, L, state.mu, yPlus)
